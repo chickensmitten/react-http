b4d6336e3683ba0af53a6385de88f86d009b4650
sheng yeong <chia.shengyeong@gmail.com>
2020-07-10 12:22:55 +0800

    Adding global interceptors from axios

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+// functions to define globally for every requests leaving and coming in. 
+// can be used for common headers like authorization header, responses, logging errors globally.
+axios.interceptors.request.use(request => {
+  // Edit the request config
+  console.log(request);
+  // need to always return the request to prevent blocking
+  return request
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+  // Edit the request config
+  console.log(response);
+  // need to always return the request to prevent blocking
+  return response;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
