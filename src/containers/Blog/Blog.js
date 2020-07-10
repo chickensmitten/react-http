6424e4b1a168443942511b710e3c4913d9fe64aa
sheng yeong <chia.shengyeong@gmail.com>
2020-07-10 11:32:08 +0800

    Transformed author data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -16,14 +16,21 @@ class Blog extends Component {
         // the problem with this method is that javascript will move on immediately, will not wait to store it
         axios.get('http://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({posts: response.data});
-                // console.log(response);
+                const posts = response.data.slice(0,4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                });
+                this.setState({posts: updatedPosts});
+                console.log(updatedPosts);
             });
     }
 
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} />;
+            return <Post key={post.id} title={post.title} author={post.author} />;
         });
 
         return (
